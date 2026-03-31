4044fd9b9ffedf820176f16a10b6928ae1269bf2
DarkKandaoMaster <2837619550@qq.com>
2026-03-31 23:02:24 +0800

    修改

---
+++ backend/algorithms/__init__.py
@@ -0,0 +1,17 @@
+import importlib
+
+ALGORITHM_MAP={ #建立前端算法名称到后端文件名的映射关系。比如前端传来"K-means"，我们就把它映射成"kmeans"，然后调用kmeans.py这个文件
+    "K-means": "kmeans",
+    "Spectral Clustering": "spectral",
+    "SNF": "snf",
+    "NEMO": "nemo"
+}
+
+def load_algorithm(algorithm_name:str): #根据传入的字符串（算法名称），动态导入对应的算法模块，返回该算法模块里面的Algorithm类
+    if algorithm_name not in ALGORITHM_MAP:
+        raise ValueError(f"暂时不支持的算法：{algorithm_name}")
+    try:
+        module=importlib.import_module(   f".{ALGORITHM_MAP[algorithm_name]}"   ,package=__name__) #使用相对导入。拼接出相对导入的字符串。package=__name__表示从当前目录开始
+        return module.Algorithm
+    except Exception as e:
+        raise RuntimeError(f"加载算法模块 {algorithm_name} 失败：{str(e)}")
--- backend/server.py
+++ backend/server.py
@@ -29,25 +29,7 @@ import json
 import asyncio
 import time
 from contextlib import asynccontextmanager
-# from algorithms import get_algorithm #
-
-# =============================================================================
-# 注册算法
-# =============================================================================
-ALGORITHM_MAP={ #建立前端算法名称到后端文件名的映射关系。比如前端传来"K-means"，我们就把它映射成"kmeans"，然后调用kmeans.py这个文件
-    "K-means": "kmeans",
-    "Spectral Clustering": "spectral",
-    "SNF": "snf",
-    "NEMO": "nemo"
-}
-def load_algorithm(algorithm_name:str): #根据传入的字符串（算法名称），动态导入对应的算法模块，返回该算法模块里面的Algorithm类
-    if algorithm_name not in ALGORITHM_MAP:
-        raise ValueError(f"暂时不支持的算法：{algorithm_name}")
-    try:
-        module=importlib.import_module(f"algorithms.{ALGORITHM_MAP[algorithm_name]}")
-        return module.Algorithm
-    except Exception as e:
-        raise RuntimeError(f"加载算法模块 {algorithm_name} 失败：{str(e)}")
+from .algorithms import load_algorithm
 
 # =============================================================================
 # 后台定时清理任务
