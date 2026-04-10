bf2099610629c79626df762512934979c7af1484
DarkKandaoMaster <2837619550@qq.com>
2026-04-10 22:20:23 +0800

    小修改

--- backend/server.py
+++ backend/server.py
@@ -29,7 +29,7 @@ import json
 import asyncio
 import time
 from contextlib import asynccontextmanager
-from algorithms import load_algorithm #导入我们在./algorithms/__init__.py里写的load_algorithm函数
+from .algorithms import load_algorithm #导入我们在./algorithms/__init__.py里写的load_algorithm函数
 
 # =============================================================================
 # 后台定时清理任务
